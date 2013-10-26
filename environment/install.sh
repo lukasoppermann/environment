9d4225486f18d240a0bc0b9a4e6df5b00a4d8616
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-10-26 09:53:55 +0200

    asking for virtual hostname

--- environment/install.sh
+++ environment/install.sh
@@ -1,5 +1,6 @@
 # Setup Dev environment
 # Ask for the administrator password upfront
+: <<'END'
 echo "This script needs Administrator privileges to edit some files in the /etc directory.";
 sudo -v
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
@@ -38,14 +39,24 @@ echo "location: $(which php)"
 echo "$(php --version)"
 
 # backup apache config file
+echo $'\n'"Backing up Apache config file..."
 sudo cp /etc/apache2/httpd.conf /etc/apache2/httpd.conf.bak
 # activate php
+echo $'\n'"Activating PHP on Apache..."
 apachecnf=$(sudo sed -e 's/#LoadModule php5_module libexec\/apache2\/libphp5.so/LoadModule php5_module libexec\/apache2\/libphp5.so/' /etc/apache2/httpd.conf)
 # enable virtual hosts
+echo $'\n'"Activating virtual hosts..."
 apachecnf=$(sudo sed -e "s/#Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/" <<< "$apachecnf")
 # create new file
 echo "$apachecnf" | sudo tee /etc/apache2/httpd.conf.tmp > /dev/null
 sudo mv /etc/apache2/httpd.conf.tmp /etc/apache2/httpd.conf
-# 
+END
+# Ask for virtual host name 
+read -p 'Enter your vhost name e.g. www or username.local ' virtualHost
+virtualHost=$(echo $virtualHost | tr '[:upper:]' '[:lower:]')
+virtualHost=$(sed "s/[^a-z.|0-9\-]//g;" <<< $virtualHost)
+echo "Your virtual Host name is: "$virtualHost
+
+
 # timeout sudo
 # sudo -k
